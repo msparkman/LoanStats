9e8dceb145627dcdc56e3ee1aa637f092a033315
Matt Sparkman <mhsparkman@gmail.com>
2024-03-29 08:42:02 -0500

    NOISSUE: Added small visual indicator for required fields. Also started work on trying to sanitize input as numbers.

--- src/app/loan-summary/loan-summary.component.ts
+++ src/app/loan-summary/loan-summary.component.ts
@@ -9,29 +9,33 @@ import { LoanMonth } from '../loan-month';
 })
 export class LoanSummaryComponent {
   loan: Loan = {
-    principal: 126000,
-    rate: 2.99,
-    termYears: 30
+    principal: "126,000.00",
+    rate: "2.99",
+    termYears: "30"
   };
 
   calculate(loan: Loan): void {
+    let principal: number = Math.round(parseFloat(loan.principal) * 100) / 100;
+
     // Monthly Rate
-    let monRate: number = loan.rate / (12 * 100);
+    let monRate: number = parseFloat(loan.rate) / (12 * 100);
     let monPay: number = 0;
 
     // If loan length is given, calculate the monthly payment
     if (loan.termYears) {
-      let termMon: number = loan.termYears * 12;
+      let termMon: number = parseInt(loan.termYears) * 12;
       // Monthly Payment
-      monPay = loan.principal * (monRate / (1 - (1 + monRate) ** -termMon));
-      loan.monthlyPayment = monPay;
+      monPay = principal * (monRate / (1 - (1 + monRate) ** -termMon));
+      loan.monthlyPayment = monPay.toString();
     } else if (loan.monthlyPayment) {
-      monPay = loan.monthlyPayment;     
+      monPay = parseFloat(loan.monthlyPayment);     
     }
-    let oldPrincipal: number = loan.principal;
+
+    let oldPrincipal: number = principal;
     let newPrincipal: number = oldPrincipal;
+    let parsedExtraPrincipal: number = parseFloat(loan.extraPrincipalPayment ? loan.extraPrincipalPayment : "0");
+
     let loanMonths: LoanMonth[] = new Array();
-    
     let numOfMonths: number = 1;
     let totalInterestPaid: number = 0;
     while (newPrincipal > 0) {
@@ -40,16 +44,12 @@ export class LoanSummaryComponent {
         // Monthly Principal Payment
         let monPrincipalPay: number = monPay - monInterestPay;
 
-        let extraPrincipalPayment: number = 0;
-        if (loan.extraPrincipalPayment && loan.extraPrincipalPayment > 0) {
-          extraPrincipalPayment = loan.extraPrincipalPayment;
-        }
-        newPrincipal = oldPrincipal - monPrincipalPay - extraPrincipalPayment;
+        newPrincipal = oldPrincipal - monPrincipalPay - parsedExtraPrincipal;
         let loanMonth: LoanMonth = {
           monthNum: numOfMonths,
           principal: oldPrincipal,
           towardsInterest: monInterestPay,
-          towardsPrincipal: +monPrincipalPay + +extraPrincipalPayment,
+          towardsPrincipal: +monPrincipalPay + +parsedExtraPrincipal,
           newPrincipal: newPrincipal
         }
         loanMonths.push(loanMonth);
--- src/app/loan.ts
+++ src/app/loan.ts
@@ -1,11 +1,11 @@
 import { LoanMonth } from "./loan-month";
 
 export interface Loan {
-    principal: number;
-    rate: number;
-    termYears?: number;
-    extraPrincipalPayment?: number;
-    monthlyPayment?: number;
+    principal: string;
+    rate: string;
+    termYears?: string;
+    extraPrincipalPayment?: string;
+    monthlyPayment?: string;
     loanMonths?: LoanMonth[];
     loanMessage?: string;
     totalInterestPaid?: number;
